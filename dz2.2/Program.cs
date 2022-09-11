cc2277a37856b3dab9d5c8113cfac39e4da64690
Arsenia <arsenia1993@mail.ru>
2022-09-11 13:28:02 +0300

    Новые домашние задания

---
+++ dz2.2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+Console.WriteLine(" Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 10;
+int c = 0;
+
+if (a>100)
+{
+    do
+    {
+    c = a%b;
+    b = b*10;
+    }
+    while (b*10<a);
+    Console.WriteLine(a);
+    Console.WriteLine(c/(b/100));
+}
+else 
+{Console.WriteLine(a);
+Console.WriteLine("третьей цифры нет");
+}
