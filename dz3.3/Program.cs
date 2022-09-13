d221978e1fb4c288ea6c5862864bc9f22830df29
Arsenia <arsenia1993@mail.ru>
2022-09-13 19:45:14 +0300

    Добавила нужное задание

--- dz3.3/Program.cs
+++ dz3.3/Program.cs
@@ -1,13 +1,12 @@
-﻿// Напишите цикл, который принимает на вход два числа (A и B) 
-// и возводит число A в натуральную степень B.
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт 
+// таблицу кубов чисел от 1 до N.
 
-int itog = 1;
-Console.WriteLine("Введите число А");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число В");
-int b = Convert.ToInt32(Console.ReadLine());
-for (int i = 0; i < b; i = i + 1)
+int N = Convert.ToInt32(Console.ReadLine());
+if (N > 0)
 {
-    itog = itog * a;
+    for (int i = 1; i <= n; i = i + 1)
+    Console.Write("{0}, ",i*i*i);
+} else
+{
+    Console.WriteLine("Введено не корректное число");
 }
-Console.WriteLine(itog);
