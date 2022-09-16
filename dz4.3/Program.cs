8b7a86fa0f2f5054e25559c79e16a8d1e12e7588
Arsenia <arsenia1993@mail.ru>
2022-09-16 19:10:40 +0300

    Добавила домашку 4.3

---
+++ dz4.3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+    int size = 8;
+    int[] V = new int[size];
+    Console.WriteLine(" Введите значение вектора ");
+    for (int i = 0; i < size; i++)
+    {
+        V[i]= Convert.ToInt32(Console.ReadLine());
+    }
+    Console.Write("[");
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("{0}, ",V[i]);
+    }
+    Console.Write("]");
