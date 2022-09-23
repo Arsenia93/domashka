fd1261b3c142fba47b02a67518df7c377ecd3617
Arsenia <arsenia1993@mail.ru>
2022-09-23 19:19:16 +0300

    Добавила шестую домашку

---
+++ dz6.1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел 
+// больше 0 ввёл пользователь.
+
+Console.WriteLine(" Введите количество чисел ");
+int M = Convert.ToInt32(Console.ReadLine());
+if (M > 0)
+{
+    int morethanzero = 0;
+    int[] numbers = new int[M];
+    Console.WriteLine(" Введите числа ");
+    for (int i = 0; i < M; i++)
+    {
+        numbers[i] = Convert.ToInt32(Console.ReadLine());
+        if (numbers[i] > 0)
+        morethanzero++; 
+    } 
+    Console.WriteLine(" Положительных чисел ");
+    Console.WriteLine(morethanzero);    
+} 
---
+++ dz6.2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных 
+// уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine(" Введите значения b1, k1, b2 и k2 ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+int k1 = Convert.ToInt32(Console.ReadLine());
+int b2 = Convert.ToInt32(Console.ReadLine());
+int k2 = Convert.ToInt32(Console.ReadLine());
+float chislitel = (b1 - b2);
+float znamenatel = (k2 - k1);
+float x = chislitel / znamenatel;
+float y = x * k1 + b1;
+Console.WriteLine("Точка пересечения двух прямых ({0};{1})", x, y);
