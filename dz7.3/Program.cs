6df488f06ad3b9ec4192914f8c952445956436d5
Arsenia <arsenia1993@mail.ru>
2022-09-29 22:25:27 +0300

    Добавила седьмую домашку

---
+++ dz7.3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.WriteLine("Введите количество строк в массиве");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в массиве");
+int n = Convert.ToInt32(Console.ReadLine());
+float[] sr = new float [n];
+int[,] massive = new int[m,n];
+
+for (int i = 0; i < n; i++)
+    {
+    sr[i] = 0;
+    }
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+    massive[i,j] = Convert.ToInt32(Console.ReadLine());
+    sr[j] = sr[j] + (float)massive[i,j]/(float)m;
+    }
+}
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+    Console.Write("{0} ", massive[i,j]);
+    }
+    Console.WriteLine();
+}
+ for (int j = 0; j < n; j++)
+    {
+    Console.Write("{0} ", Math.Round(sr[j],1));
+    }
